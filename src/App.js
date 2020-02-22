f624f53f0da0520f470e33066da7348061ee9062
Connor Claxton <czclaxton@gmail.com>
2020-02-22 15:04:27 -0600

    get request setup and displaying data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import './App.scss';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import axios from "axios";
 
 function App() {
+  const [comic, setComic] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json")
+      .then(res => setComic(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  console.log("comic", comic);
+
+  if (!comic) {
+    return <h1 className="app">Loading...</h1>;
+  }
+
   return (
     <div className="app">
-      React XKCD
+      <h1>{comic.title}</h1>
+      <img src={comic.img} />
+      <p>{comic.alt}</p>
     </div>
   );
-};
+}
 
 export default App;
